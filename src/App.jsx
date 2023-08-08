e0b479b87d12bbe89e08ffa730ad2bcbdc935995
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-08-09 00:44:08 +0300

    updating (Dynamic Routes wiht URL, Programmatic Navigation)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -7,8 +13,9 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 
 import CityList from "./components/CityList";
+import City from "./components/City";
 import CountryList from "./components/CountryList";
-import { useEffect, useState } from "react";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -35,21 +42,22 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
+          <Route index element={<Navigate to="cities" replace />} />
           <Route
-            index
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,23 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  // console.log(searchParams.get("lat"));
+  // console.log(searchParams.get("lng"));
+  const [lat, lng] = [searchParams.get("lat"), searchParams.get("lng")];
+  const navigate = useNavigate();
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h1>
+        Position:: {lat},{lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 20, lng: 43 })}>
+        Change URL
+      </button>
+    </div>
+  );
 }
 
 export default Map;
